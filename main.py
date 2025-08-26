d9adacbe4210cb823cfbb3efff5ad2991e1d2640
abraao7 <aesemiliano@outlook.com>
2025-08-25 21:36:40 -0300

    Main.py do projeto

---
+++ main.py
@@ -0,0 +1,13 @@
+from tkinter import Tk
+
+class app:
+    '''classe principal'''
+    def __init__(self):
+        janela = Tk()
+        janela.title('CEPI')
+        janela.geometry('500x300')
+        janela.mainloop()
+
+    print('Vamos começar!')
+
+program = app()
